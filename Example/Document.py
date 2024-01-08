89016a925ba3e855625c6f2a87c8e49fcf39784a
Parvez Khan <parvezkhan0802@gmail.com>
2024-01-08 21:42:34 +0530

    Refactor: Remove unnecessary whats.open_web in examples

--- Example/Document.py
+++ Example/Document.py
@@ -2,8 +2,5 @@
 
 from whatpack.synchronous import whats
 
-# Open WhatsApp Web
-whats.open_web()
-
 # Send a document instantly
 whats.send_whats_doc_immediately(phone_no="+1234567890", path="/path/to/document.pdf", message="Check this document!")
--- Example/Group.py
+++ Example/Group.py
@@ -2,8 +2,5 @@
 
 from whatpack.synchronous import whats
 
-# Open WhatsApp Web
-whats.open_web()
-
 # Send a message to a group instantly
 whats.send_what_msg_to_group_instantly(group_id="your_group_id", message="Hello, group!")
